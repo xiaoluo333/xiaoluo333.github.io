dd69409d82443914bc0b71bc567e36aafc6b83a6
xiaoluo333 <1830255169@qq.com>
2023-11-26 18:43:47 +0800

    Site updated: 2023-11-26 18:43:43

---
+++ js/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [
+        {
+            name: 'cornfield chase',
+            artist: 'Hans Zimmer',
+            url: '/music/cornfield chase.mp3',
+            cover: '/music/cornfield chase.jpg'
+        }
+    ]
+});
