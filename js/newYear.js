072ddd081e6cb452b084a1556c2e92e5b24bbed5
xiaoluo333 <1830255169@qq.com>
2023-12-07 21:31:19 +0800

    Site updated: 2023-12-07 21:31:10

--- js/newYear.js
+++ js/newYear.js
@@ -58,8 +58,8 @@ var newYear = () => {
         });
     });
 }
-newYear()
+//newYear()
 // Pjax适配：若没有开启Pjax这里直接是newYear()即可
 // 开了Pjax的用以下两句
-// document.addEventListener('pjax:complete', newYear);
-// document.addEventListener('DOMContentLoaded', newYear);
+document.addEventListener('pjax:complete', newYear);
+document.addEventListener('DOMContentLoaded', newYear);
